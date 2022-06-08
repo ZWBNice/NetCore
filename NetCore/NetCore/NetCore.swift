fecf1f0516fb5850caa7db58f7592c84cf74fce3
zwb <wenbo.zhang@imtpath.com>
2022-06-08 13:33:16 +0800

    firstcommit

---
+++ NetCore/NetCore/NetCore.swift
@@ -0,0 +1,50 @@
+//
+//  NetCore.swift
+//  NetCore
+//
+//  Created by 张文博 on 2022/6/7.
+//
+
+import Foundation
+import UIKit
+public class NetCore: NSObject {
+    
+    // 外部调用 可以在oc 和 swift 中同时使用
+    @objc public func request(){
+        print("请求")
+    }
+
+    // 内部调用
+    private func privateRequest(){
+        print("私有请求")
+    }
+    
+    // 作用域外可以继承 可访问可覆盖
+    open func openRequest(){
+        print("open请求")
+
+    }
+    
+    open func openRequest1(){
+        print("open请求1")
+
+    }
+    
+    open func openRequest2(){
+        print("open请求2")
+
+    }
+    
+    open func openRequest3(){
+        print("open请求3")
+
+    }
+    open func openRequest4(){
+        print("open请求3")
+
+    }
+
+
+
+
+}
